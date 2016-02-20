201389dd4cdca442e4d5791b7cf8aceba9d1e6b4
Nikita Tsvetkov <nikitanovosibirsk@yandex.com>
2016-02-20 15:02:44 +0500

    initial commit

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+
+setup(
+  name='jj',
+  description='',
+  version='0.1.0',
+  url='https://github.com/nikitanovosibirsk/jj',
+  author='Nikita Tsvetkov',
+  author_email='nikitanovosibirsk@yandex.com',
+  license='MIT',
+  packages=find_packages()
+)
