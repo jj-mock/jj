24aa23e6ef21cc8ea36d625255dd7d0758438bb4
Nikita Tsvetkov <nikitanovosibirsk@yandex.com>
2019-02-27 18:21:01 +0700

    update requests lib

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 setup(
     name='jj',
     description='A simple testing tool for mocking HTTP responses',
-    version='0.1.3',
+    version='0.1.4',
     url='https://github.com/nikitanovosibirsk/jj',
     author='Nikita Tsvetkov',
     author_email='nikitanovosibirsk@yandex.com',
@@ -11,7 +11,7 @@ setup(
     packages=find_packages(),
     install_requires=[
         'Werkzeug==0.14.1',
-        'requests==2.9.1',
-        'requests-toolbelt==0.6.0'
+        'requests==2.21.0',
+        'requests-toolbelt==0.9.1'
     ]
 )
