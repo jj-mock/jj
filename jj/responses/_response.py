cd58406620b0118173f61b98f0ffc183fa8735fa
Nikita Tsvetkov <nikitanovosibirsk@yandex.com>
2019-07-06 13:34:02 +0700

    add packing for response

--- jj/responses/_response.py
+++ jj/responses/_response.py
@@ -1,16 +1,17 @@
 import io
-from typing import Any, Optional, Union, MutableMapping
 from json import dumps
+from typing import Any, Dict, MutableMapping, Optional, Union
 from unittest.mock import sentinel as nil
 
 from aiohttp import web
+from packed import packable
 
 from ._stream_response import StreamResponse
-
 
 __all__ = ("Response",)
 
 
+@packable("jj.responses.Response")
 class Response(web.Response, StreamResponse):
     def __init__(self, *,
                  json: Any = nil,
@@ -33,3 +34,21 @@ class Response(web.Response, StreamResponse):
             headers.update({"Content-Disposition": "inline"})
 
         super().__init__(body=body, text=text, status=status, reason=reason, headers=headers)
+
+    def __packed__(self) -> Dict[str, Any]:
+        headers = [[key, val] for key, val in self.headers.items()]
+        return {
+            "status": self.status,
+            "reason": self.reason,
+            "headers": headers,
+            "body": self.body,
+        }
+
+    @classmethod
+    def __unpacked__(cls, *,
+                     body: Union[str, bytes],
+                     status: int,
+                     reason: str,
+                     headers: MutableMapping[str, str],
+                     **kwargs: Any) -> "Response":
+        return cls(body=body, status=status, reason=reason, headers=headers)
