b48d533aa16d1e41a26abaaeeada3ce83d7359ab
Daria Cherepakhina <da19sha96@yandex.ru>
2022-03-28 01:36:51 +0300

    added tests

--- tests/expiration_policy/test_expiration_policy.py
+++ tests/expiration_policy/test_expiration_policy.py
@@ -4,7 +4,6 @@ import jj
 from jj.expiration_policy import ExpireAfterRequests, ExpireNever
 from jj.middlewares import SelfMiddleware
 from jj.mock import Mock, RemoteMock
-
 from .._test_utils import run
 
 
@@ -58,6 +57,26 @@ async def test_expire_after_requests(count_requests: int):
 
         excess_response_body = await response.read()
         assert excess_response_body == b"text"
+
+
+@pytest.mark.asyncio
+@pytest.mark.parametrize("count_requests", [0, -1])
+async def test_expire_after_requests_with_invalid_count_requests(count_requests: int):
+    mock = Mock()
+    self_middleware = SelfMiddleware(mock.resolver)
+    matcher, response = jj.match("*"), jj.Response(status=200, body=b"text")
+    policy = ExpireAfterRequests(-1)
+
+    async with run(mock, middlewares=[self_middleware]) as client:
+        remote_mock = RemoteMock(client.make_url("/"))
+        handler = remote_mock.create_handler(matcher, response, expiration_policy=policy)
+        await handler.register()
+
+        response = await client.get("/")
+        assert response.status == 404
+
+        response_body = await response.read()
+        assert response_body == b""
 
 
 @pytest.mark.asyncio
