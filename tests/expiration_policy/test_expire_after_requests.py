5f6ae9a2e3357bac51c224257e903b1fb3e26b6c
Daria Cherepakhina <da19sha96@yandex.ru>
2022-03-28 02:32:11 +0300

    added tests

---
+++ tests/expiration_policy/test_expire_after_requests.py
@@ -0,0 +1,40 @@
+import pytest
+
+from jj.expiration_policy import ExpireAfterRequests
+
+from .._test_utils.steps import given, then, when
+
+
+def test_not_expired_after_requests():
+    with given:
+        expiration_policy = ExpireAfterRequests(1)
+
+    with when:
+        is_expired = expiration_policy.is_expired(None)
+
+    with then:
+        assert not is_expired
+
+
+def test_expired_after_requests():
+    with given:
+        expiration_policy = ExpireAfterRequests(1)
+        expiration_policy.is_expired(None)
+
+    with when:
+        is_expired = expiration_policy.is_expired(None)
+
+    with then:
+        assert is_expired
+
+
+@pytest.mark.parametrize("count_requests", [0, -1])
+def test_expired_after_requests_with_invalid_count_requests(count_requests: int):
+    with given:
+        expiration_policy = ExpireAfterRequests(count_requests)
+
+    with when:
+        is_expired = expiration_policy.is_expired(None)
+
+    with then:
+        assert is_expired
