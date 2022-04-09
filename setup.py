39eb3a9e4c871ece8324afaca1f6d86a2acdf868
Nikita Tsvetkov <nikitanovosibirsk@yandex.com>
2022-04-09 15:12:30 +0300

    bump version → 2.4.3

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ def find_dev_required():
 
 setup(
     name="jj",
-    version="2.4.2",
+    version="2.4.3",
     description="Remote HTTP Mock",
     long_description=open("README.md").read(),
     long_description_content_type="text/markdown",
