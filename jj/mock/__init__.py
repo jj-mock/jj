c167d0ad62013672de45899918ab55cdd3b0bf11
Nikita <nikitanovosibirsk@yandex.com>
2022-11-25 18:16:30 +0400

    add stacked (#41)

--- jj/mock/__init__.py
+++ jj/mock/__init__.py
@@ -18,10 +18,15 @@ from ._mocked import Mocked
 from ._remote_handler import RemoteHandler
 from ._remote_mock import RemoteMock
 from ._remote_response import RemoteResponseType
+from ._stacked import stacked
 from ._system_log_filter import SystemLogFilter
 
-_REMOTE_MOCK_URL = os.environ.get("JJ_REMOTE_MOCK_URL", "http://localhost:8080")
-_REMOTE_MOCK_DISPOSABLE = os.environ.get("JJ_REMOTE_MOCK_DISPOSABLE", "True")
+REMOTE_MOCK_URL = os.environ.get("JJ_REMOTE_MOCK_URL", "http://localhost:8080")
+REMOTE_MOCK_DISPOSABLE = os.environ.get("JJ_REMOTE_MOCK_DISPOSABLE", "True")
+
+# backward compatibility
+_REMOTE_MOCK_URL = REMOTE_MOCK_URL
+_REMOTE_MOCK_DISPOSABLE = REMOTE_MOCK_DISPOSABLE
 
 
 def mocked(matcher: Union[RequestMatcher, LogicalMatcher],
@@ -32,18 +37,25 @@ def mocked(matcher: Union[RequestMatcher, LogicalMatcher],
            prefetch_history: bool = True,
            history_adapter: Optional[HistoryAdapterType] = default_history_adapter) -> "Mocked":
     if disposable is None:
-        disposable = bool(strtobool(_REMOTE_MOCK_DISPOSABLE))
+        disposable = bool(strtobool(REMOTE_MOCK_DISPOSABLE))
 
-    handler = RemoteMock(_REMOTE_MOCK_URL).create_handler(
-        matcher,
-        response,
-        expiration_policy,
-        history_adapter=history_adapter
-    )
+    handler = create_remote_handler(matcher, response, expiration_policy,
+                                    history_adapter=history_adapter)
     return Mocked(handler, disposable=disposable, prefetch_history=prefetch_history)
 
 
-__all__ = ("Mock", "mocked", "RemoteMock", "RemoteHandler", "Mocked",
-           "HistoryRepository", "HistoryRequest", "HistoryResponse", "HistoryItem",
+def create_remote_handler(matcher: Union[RequestMatcher, LogicalMatcher],
+                          response: RemoteResponseType,
+                          expiration_policy: Optional[ExpirationPolicy] = None,
+                          *,
+                          mock_url: str = REMOTE_MOCK_URL,
+                          history_adapter: Optional[HistoryAdapterType] = default_history_adapter
+                          ) -> RemoteHandler:
+    return RemoteMock(mock_url).create_handler(matcher, response, expiration_policy,
+                                               history_adapter=history_adapter)
+
+
+__all__ = ("Mock", "mocked", "stacked", "create_remote_handler", "RemoteMock", "RemoteHandler",
+           "Mocked", "HistoryRepository", "HistoryRequest", "HistoryResponse", "HistoryItem",
            "SystemLogFilter", "RemoteResponseType", "HistoryAdapterType",
-           "default_history_adapter",)
+           "default_history_adapter", "REMOTE_MOCK_URL", "REMOTE_MOCK_DISPOSABLE",)
