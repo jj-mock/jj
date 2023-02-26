b8540f7c9dc630ed9d51952e45fb75ebeaad20db
Nikita Tsvetkov <tsv1@fastmail.com>
2023-02-26 19:49:30 +0400

    bump version → 2.7.2

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ def find_dev_required():
 
 setup(
     name="jj",
-    version="2.7.1",
+    version="2.7.2",
     description="Remote HTTP Mock",
     long_description=open("README.md").read(),
     long_description_content_type="text/markdown",
