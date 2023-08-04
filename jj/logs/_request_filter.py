6b4f91630eace3e80bd5aa57e07567e31d638b1d
Nikita Tsvetkov <tsv1@fastmail.com>
2023-08-04 22:08:51 +0400

    add request filter

---
+++ jj/logs/_request_filter.py
@@ -0,0 +1,13 @@
+import logging
+from logging import LogRecord
+
+__all__ = ("RequestFilter",)
+
+
+class RequestFilter(logging.Filter):
+    def filter(self, record: LogRecord) -> bool:
+        request = getattr(record, "jj_request", None)
+        response = getattr(record, "jj_response", None)
+        if (request is not None) and (response is None):
+            return False
+        return super().filter(record)
