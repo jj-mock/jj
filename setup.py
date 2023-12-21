12e051ac07c24b2b2b7fba9432b7cf83ce5945fc
Nikita Tsvetkov <tsv1@fastmail.com>
2023-12-21 22:40:56 +0400

    update classifier

--- setup.py
+++ setup.py
@@ -28,7 +28,6 @@ setup(
     tests_require=find_dev_required(),
     classifiers=[
         "License :: OSI Approved :: Apache Software License",
-        "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
         "Programming Language :: Python :: 3.10",
