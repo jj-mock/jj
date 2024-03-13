8a0b618760b51ee5bda82f5915ebff074e0ffebf
Nikita Tsvetkov <tsv1@fastmail.com>
2024-03-13 20:52:11 +0400

    add handlers

--- jj/mock/_mock.py
+++ jj/mock/_mock.py
@@ -1,12 +1,15 @@
+import json
 from typing import Any, Callable, Optional, Tuple, Union
 
 from packed import pack, unpack
+from yarl import URL
 
 import jj
 from jj import default_app, default_handler
 from jj.apps import BaseApp, create_app
 from jj.expiration_policy import ExpirationPolicy
 from jj.http.codes import BAD_REQUEST, OK
+from jj.http.headers import CONTENT_TYPE
 from jj.http.methods import ANY, DELETE, GET, POST
 from jj.matchers import LogicalMatcher, RequestMatcher, ResolvableMatcher, exists
 from jj.requests import Request
@@ -77,6 +80,9 @@ class Mock(jj.App):
 
         self._resolver.register_attribute("handler_id", handler_id, handler)
         self._resolver.register_attribute("expiration_policy", expiration_policy, handler)
+        self._resolver.register_attribute("matcher", matcher, handler)
+        self._resolver.register_attribute("response", response, handler)
+
         setattr(self._app.__class__, handler_id, matcher(handler))
 
     def _deregister_handler(self, handler_id: str) -> None:
@@ -161,6 +167,72 @@ class Mock(jj.App):
         history = await self._repo.get_by_tag(handler_id)
         packed = pack(history)
         return Response(status=OK, body=packed)
+
+    def _get_base_url(self, request_url: URL) -> str:
+        base_url = f"{request_url.scheme}://{request_url.host}"
+        if request_url.port not in (80, 443):
+            base_url += f":{request_url.port}"
+        return base_url
+
+    def _pack_value(self, value: Any) -> Any:
+        if hasattr(value, "__packed__"):
+            return {value.__class__.__name__: self._pack_value(value.__packed__())}
+        elif isinstance(value, dict):
+            return {k: self._pack_value(v) for k, v in value.items()}
+        elif isinstance(value, (list, tuple)):
+            return [self._pack_value(v) for v in value]
+        else:
+            return value
+
+    def _pack_to_json(self, value: Any) -> str:
+        packed = self._pack_value(value)
+        return json.dumps(packed, default=str, ensure_ascii=False, indent=4)
+
+    @jj.match(GET, "/__jj__")
+    async def api_index(self, request: Request) -> Response:
+        base_url = self._get_base_url(request.url)
+        return jj.Response(status=OK, json={
+            "handlers": {
+                "url": f"{base_url}/__jj__/handlers",
+            }
+        })
+
+    @jj.match(GET, "/__jj__/handlers")
+    async def api_handlers(self, request: Request) -> Response:
+        base_url = self._get_base_url(request.url)
+        payload = []
+
+        handlers = self._resolver.get_handlers(self._app.__class__)
+        for handler in reversed(handlers):
+            handler_id = self._resolver.get_attribute("handler_id", handler)
+            # latest on the top
+            payload.append({
+                "handler_id": handler_id,
+                "expiration_policy": self._resolver.get_attribute("expiration_policy", handler),
+                "matcher": self._resolver.get_attribute("matcher", handler),
+                "response": self._resolver.get_attribute("response", handler),
+                "history": {
+                    "url": f"{base_url}/__jj__/handlers/{handler_id}",
+                }
+            })
+
+        return Response(status=OK, body=self._pack_to_json(payload), headers={
+            CONTENT_TYPE: "application/json"
+        })
+
+    @jj.match(GET, "/__jj__/handlers/{handler_id}")
+    async def api_history(self, request: Request) -> Response:
+        handler_id = request.segments["handler_id"]
+        history = await self._repo.get_by_tag(handler_id)
+
+        payload = []
+        for history_item in history:
+            # latest on the top
+            payload.append(history_item)
+
+        return Response(status=OK, body=self._pack_to_json(payload), headers={
+            CONTENT_TYPE: "application/json"
+        })
 
     @jj.match(ANY)
     async def resolve(self, request: Request) -> StreamResponse:
