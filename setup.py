3cc2ff5e4f4422a934b1d6af14c06cb0b5092393
Nikita Tsvetkov <tsv1@fastmail.com>
2025-05-25 12:19:11 +0400

    Remove Python 3.8 from setup.py and test matrix in workflow

--- setup.py
+++ setup.py
@@ -35,11 +35,11 @@ setup(
     tests_require=find_dev_required(),
     classifiers=[
         "License :: OSI Approved :: Apache Software License",
-        "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
         "Programming Language :: Python :: 3.10",
         "Programming Language :: Python :: 3.11",
         "Programming Language :: Python :: 3.12",
+        "Programming Language :: Python :: 3.13",
         "Typing :: Typed",
     ],
 )
