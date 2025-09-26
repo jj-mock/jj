eba510c121aa572a709f9a7f89987075096f3392
Nikita Tsvetkov <tsv1@fastmail.com>
2025-09-26 19:41:12 +0400

    bump version → 2.13.0

--- jj/_version.py
+++ jj/_version.py
@@ -1,6 +1,6 @@
 import aiohttp
 
-version = "2.12.1"
+version = "2.13.0"
 server_version = "jj/{} via aiohttp/{}".format(version, aiohttp.__version__)
 
 
--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ def find_dev_required():
 
 setup(
     name="jj",
-    version="2.12.1",
+    version="2.13.0",
     description="Remote HTTP Mock",
     long_description=open("README.md").read(),
     long_description_content_type="text/markdown",
